5afd273eff917b5792aa16da595ff15ed27366c2
DaGolle <dagolle@telenet.be>
2017-11-11 18:02:54 +0100

    First working version
    To do:
    - remove a lot of code duplication
    - add option for inputs as motion detectors

---
+++ index.js
@@ -0,0 +1,18 @@
+const OpenMoticsPlatform = require('./lib/OpenMoticsPlatform');
+const packageJson = require('./package.json');
+
+module.exports = function(homebridge) {
+  console.log("homebridge API version: " + homebridge.version);
+
+  // Accessory must be created from PlatformAccessory Constructor
+  Accessory = homebridge.platformAccessory;
+
+  // Service and Characteristic are from hap-nodejs
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  UUIDGen = homebridge.hap.uuid;
+  
+  // For platform plugin to be considered as dynamic platform plugin,
+  // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
+  homebridge.registerPlatform("homebridge-openmotics", "OpenMotics", OpenMoticsPlatform, false);
+}
